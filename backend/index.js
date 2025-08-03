1a1f5a8ed73545c1586d283f3bb77fe4e13144c8
Tannu Kumari <118298040+tannuk716@users.noreply.github.com>
2025-08-04 02:28:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"https://ai-virtual-assistant-frontend-g427.onrender.com",
+    origin:
     "https://ai-virtual-assistant-gamma.vercel.app",
     credentials:true
 }))
